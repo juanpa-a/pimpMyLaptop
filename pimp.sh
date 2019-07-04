eb60ad0ee3ad188a249fdedf30087a26fa756df8
Juan Pablo <jpabloascencioglez@gmail.com>
2019-07-04 00:10:54 -0500

    <3

--- pimp.sh
+++ pimp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo mv ePapirus/ /usr/share/icons/
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
+
+sudo mv papirus-icon-theme/ePapirus/ /usr/share/icons/
 sudo mv ProductSans/ /usr/share/fonts/
 
 sudo apt install gnome-tweaks
 gnome-tweaks
-
- 
